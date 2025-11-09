f9cb09c9c77f56878713cbd8729294e415ba201b
Bipan Gurung <bipan.gurung1124@pcmgmt.edu.np>
2025-11-10 00:28:55 +0545

    Adding Delete feature

--- src/component/ToDoApp.jsx
+++ src/component/ToDoApp.jsx
@@ -3,30 +3,44 @@ import "./ToDoApp.css";
 
 export default function ToDoApp() {
   const [tasks, setTasks] = useState([]);
-  const [newTask, setNewTask] = useState([""]);
+  const [input, setInput] = useState([""]);
 
   const handleAddTask = () => {
-    if (!newTask.trim()) return;
+    if (input.trim() === "") return;
 
-    setTasks([...tasks, newTask]);
-    setNewTask("");
+    setTasks([...tasks, input]);
+    setInput("");
+  };
+  const HandleDeletetask = (index) => {
+    const updatedTaks = tasks.filter((_, i) => i != index);
+    setTasks(updatedTaks);
   };
   return (
     <div className="todoContainer">
+      <h2>My To-Do-list</h2>
+
       <div classNam="input-area">
         <input
           type="text"
           placeholder="Add new task..."
-          value={newTask}
-          onChange={(e) => setNewTask(e.target.value)}
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
         />
         <button onClick={handleAddTask}>Add Task </button>
+      </div>
+
+      {tasks.length > 0 ? (
         <ul className="task-list">
           {tasks.map((task, index) => (
-            <li key={index}>{task}</li>
-          ))}{" "}
+            <li key={index}>
+              {task}
+              <button onClick={() => HandleDeletetask(index)}>Delete</button>
+            </li>
+          ))}
         </ul>
-      </div>
+      ) : (
+        <p>No Tasks yet</p>
+      )}
     </div>
   );
 }
